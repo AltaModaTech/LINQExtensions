c421ce4afaf34a086c6c15c309ba9ca5090a3cd1
J Burnett <jburnett@appliedsystems.com>
2016-12-27 14:41:34 -0500

    Removed GuidAttribute in AssemblyInfo enabling support back to netstandard1.0

--- AMT.LinqExtensions/Properties/AssemblyInfo.cs
+++ AMT.LinqExtensions/Properties/AssemblyInfo.cs
@@ -15,5 +15,3 @@ using System.Runtime.InteropServices;
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-[assembly: Guid("4BE024F4-EAB6-4639-B8DE-5A93F25C5435")]
